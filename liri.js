113a340ddc52fa761660d4c6a784f1ba4f1c1dc2
atpdev14 <tproctor4414@hotmail.com>
2017-11-19 14:53:22 -0800

    Spotify and Movie search functionality added.

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var request = require("request"); 
 var keys = require("./keys.js");
 var inq = require("inquirer");
 var twitter = require("twitter");
@@ -8,6 +9,13 @@ var twitterKeys =  new twitter({
 	access_token_key: keys.twitterKeys.access_token_key,
 	access_token_secret: keys.twitterKeys.access_token_secret
 });
+var spotify = require("node-spotify-api");
+var spotifyKeys = new spotify({
+		id: keys.spotifyKeys.id,
+		secret: keys.spotifyKeys.secret
+});
+
+var omdb = require("omdb");
 
 inq.prompt([
 	{
@@ -38,7 +46,31 @@ inq.prompt([
 			// ==========================
 
 			case "Look up a song on Spotify":
-				console.log("Logging from switch- Spotify");
+				inq.prompt([
+					{
+						type: "input",
+						message: "What song would you like to search for?",
+						name: "searchInput"
+					}
+					]).then(function(response){
+						if(response.searchInput === ""){response.searchInput = "The Sign Ace of Base";}
+						spotifyKeys.search({ type: 'track', query: response.searchInput }, function(err, data) {
+  							if (err) {
+    							return console.log('Error occurred: ' + err);
+  							}
+							for(var i=0; i < 10; i++){
+
+								var songInfo = {
+									songName: data.tracks.items[i].name,
+									artist: data.tracks.items[i].album.artists[0].name,
+									album: data.tracks.items[i].album.name,
+									url: data.tracks.items[i].external_urls.spotify
+								};
+
+							console.log("Song Info: " + JSON.stringify(songInfo, null, 2));
+							};
+						})
+					});
 			break;
 			
 			// ==========================
@@ -46,7 +78,30 @@ inq.prompt([
 			// ==========================
 
 			case "Look up a movie":
-				console.log("Logging from switch- Movie");
+				inq.prompt([
+					{
+						type: "input",
+						message: "What movie would you like to search for?",
+						name: "searchInput"
+					}
+					]).then(function(response){
+						if(response.searchInput === ""){
+							response.searchInput = "Popeye";
+						}
+						request("http://www.omdbapi.com/?t=" + response.searchInput + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+							var movieInfo = JSON.parse(body);
+							console.log("Title: " + movieInfo.Title);
+							console.log("Released: " + movieInfo.Released);
+							console.log("IMDB Rating: " + movieInfo.Ratings[0].Value);
+							console.log("Rotten Tomatoes Rating: " + movieInfo.Ratings[1].Value);
+							console.log("Country of origin: " + movieInfo.Country);
+							console.log("Language: " + movieInfo.Language);
+							console.log("Plot: " + movieInfo.Plot);
+							console.log("Actors: " + movieInfo.Actors); 
+							console.log("\n");
+							console.log("\n");
+						});
+					});
 			break;
 			
 			// ==========================
